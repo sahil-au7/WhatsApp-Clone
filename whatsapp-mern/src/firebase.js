b6eb90e7f8c44b6eda943f757c44c055ef4b0c12
sahil-au7 <sahilyadav601@gmail.com>
2020-12-29 12:18:14 +0530

    sidebar and chat component is done

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7cDnJnSVzaDIvoqUHuiJqzm4-63pfDsg",
+  authDomain: "whatsapp-mern-37626.firebaseapp.com",
+  projectId: "whatsapp-mern-37626",
+  storageBucket: "whatsapp-mern-37626.appspot.com",
+  messagingSenderId: "1029064777486",
+  appId: "1:1029064777486:web:8b4147b8d3208edec5815f"
+};
